43ac4adc8c4fb406b2f7b756c28cb410ec1ff719
Bruce Bowyer-Smyth <contact@wmf2wpf.com>
2014-05-17 14:28:19 +1000

    Meta data

--- HttpModules/Properties/AssemblyInfo.cs
+++ HttpModules/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HttpModules")]
+[assembly: AssemblyProduct("SecureDefaults HttpModules")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- HttpModulesSsl/Properties/AssemblyInfo.cs
+++ HttpModulesSsl/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HttpModulesSsl")]
+[assembly: AssemblyProduct("SecureDefaults HttpModulesSsl")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
