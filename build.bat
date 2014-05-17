032b116e269b001597c8ce258ddb6a28579d61e6
Bruce Bowyer-Smyth <contact@wmf2wpf.com>
2014-05-17 14:28:43 +1000

    Added release project compile

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
+msbuild SecureDefaults.sln /t:clean /p:Configuration=Release 
+msbuild SecureDefaults.sln /p:Configuration=Release /p:VisualStudioVersion=12.0
 md builds
 md builds\local
 .\.NuGet\nuget.exe pack NuSpec\SecureDefault.nuspec -OutputDirectory builds\local
