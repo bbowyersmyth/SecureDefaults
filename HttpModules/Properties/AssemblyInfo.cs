380c1a6e2463db5f6ec46de5c0d08622310031fe
Bruce Bowyer-Smyth <contact@wmf2wpf.com>
2014-06-07 19:12:50 +1000

    Namespace correction

--- HttpModules/Properties/AssemblyInfo.cs
+++ HttpModules/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HttpModules")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SecureDefaults HttpModule")]
+[assembly: AssemblyDescription("Removes unnecessary http response headers")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SecureDefaults HttpModules")]
--- HttpModulesSsl/SslOnlyModule.cs
+++ HttpModulesSsl/SslOnlyModule.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Web;
 
-namespace SecureDefaults
+namespace SecureDefaultsSsl
 {
     public class SslOnlyModule : IHttpModule
     {
