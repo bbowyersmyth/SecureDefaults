1ce7033498c243e3c86fbdd027c23080d85b9b71
Bruce Bowyer-Smyth <contact@wmf2wpf.com>
2014-06-07 19:13:28 +1000

    Any CPU build

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
-msbuild SecureDefaults.sln /t:clean /p:Configuration=Release 
-msbuild SecureDefaults.sln /p:Configuration=Release /p:VisualStudioVersion=12.0
+msbuild SecureDefaults.sln /t:clean /p:Configuration=Release /p:Platform="Any CPU"
+msbuild SecureDefaults.sln /p:Configuration=Release /p:Platform="Any CPU" /p:VisualStudioVersion=12.0
 md builds
 md builds\local
 .\.NuGet\nuget.exe pack NuSpec\SecureDefault.nuspec -OutputDirectory builds\local
